33aaccb35564e8e2ac80ba9158d7c1bd64b7cfe9
chektek <zhoherchak@gmail.com>
2021-10-15 19:55:41 -0400

    mvp complete

--- index.js
+++ index.js
@@ -1,77 +1,80 @@
 const windowsKeys = `
+<a href="#" id="show" class="show-hide">Show Shortcuts</a>
 <div id="medium-shortcuts" class="fade-in">
+  <a href="#" id="hide" class="show-hide">Hide</a>
   <div class="drawer-content u-alignBlock js-drawerTip">
     <span class="title">Keyboard shortcuts</span>
-    <table class="drawer-list">
-      <tbody>
-        <tr>
-          <td class="drawer-listCell drawer-listCell--keyboard">
-            <span class="drawer-keyboardKey">Ctrl</span> +
-            <span class="drawer-keyboardKey">Alt</span> +
-            <span class="drawer-keyboardKey">1</span>
-          </td>
-          <td class="drawer-listCell">Bigger header or title</td>
-        </tr>
-        <tr>
-          <td class="drawer-listCell drawer-listCell--keyboard">
-            <span class="drawer-keyboardKey">Ctrl</span> +
-            <span class="drawer-keyboardKey">Alt</span> +
-            <span class="drawer-keyboardKey">2</span>
-          </td>
-          <td class="drawer-listCell">Smaller header or subtitle</td>
-        </tr>
-        <tr>
-          <td class="drawer-listCell drawer-listCell--keyboard">
-            <span class="drawer-keyboardKey">Ctrl</span> +
-            <span class="drawer-keyboardKey">Alt</span> +
-            <span class="drawer-keyboardKey">5</span>
-          </td>
-          <td class="drawer-listCell">
-            Quote<span class="drawer-annotation">Two styles of quotes </span>
-          </td>
-        </tr>
-        <tr>
-          <td class="drawer-listCell drawer-listCell--keyboard">
-            <span class="drawer-keyboardKey">Ctrl</span> +
-            <span class="drawer-keyboardKey">B</span>
-          </td>
-          <td class="drawer-listCell">Bold</td>
-        </tr>
-        <tr>
-          <td class="drawer-listCell drawer-listCell--keyboard">
-            <span class="drawer-keyboardKey">Ctrl</span> +
-            <span class="drawer-keyboardKey">I</span>
-          </td>
-          <td class="drawer-listCell">Italic</td>
-        </tr>
-        <tr>
-          <td class="drawer-listCell drawer-listCell--keyboard">
-            <span class="drawer-keyboardKey">*</span>
-            <span class="drawer-keyboardKey">Space</span>
-          </td>
-          <td class="drawer-listCell">Bulleted list</td>
-        </tr>
-        <tr>
-          <td class="drawer-listCell drawer-listCell--keyboard">
-            <span class="drawer-keyboardKey">Ctrl</span> +
-            <span class="drawer-keyboardKey">K</span>
-          </td>
-          <td class="drawer-listCell">
-            Turn into a link<span class="drawer-annotation"
-              >Works for text and images
-            </span>
-          </td>
-        </tr>
-        <tr>
-          <td class="drawer-listCell drawer-listCell--keyboard">
-            <span class="drawer-keyboardKey">1</span>
-            <span class="drawer-keyboardKey">.</span>
-            <span class="drawer-keyboardKey">Space</span>
-          </td>
-          <td class="drawer-listCell">Ordered list</td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="table-container">
+      <table class="drawer-list">
+        <tbody>
+          <tr>
+            <td class="drawer-listCell drawer-listCell--keyboard">
+              <span class="drawer-keyboardKey">Ctrl</span> +
+              <span class="drawer-keyboardKey">Alt</span> +
+              <span class="drawer-keyboardKey">1</span>
+            </td>
+            <td class="drawer-listCell">Bigger header or title</td>
+          </tr>
+          <tr>
+            <td class="drawer-listCell drawer-listCell--keyboard">
+              <span class="drawer-keyboardKey">Ctrl</span> +
+              <span class="drawer-keyboardKey">Alt</span> +
+              <span class="drawer-keyboardKey">2</span>
+            </td>
+            <td class="drawer-listCell">Smaller header or subtitle</td>
+          </tr>
+          <tr>
+            <td class="drawer-listCell drawer-listCell--keyboard">
+              <span class="drawer-keyboardKey">Ctrl</span> +
+              <span class="drawer-keyboardKey">Alt</span> +
+              <span class="drawer-keyboardKey">5</span>
+            </td>
+            <td class="drawer-listCell">
+              Quote<span class="drawer-annotation">Two styles of quotes </span>
+            </td>
+          </tr>
+          <tr>
+            <td class="drawer-listCell drawer-listCell--keyboard">
+              <span class="drawer-keyboardKey">Ctrl</span> +
+              <span class="drawer-keyboardKey">B</span>
+            </td>
+            <td class="drawer-listCell">Bold</td>
+          </tr>
+          <tr>
+            <td class="drawer-listCell drawer-listCell--keyboard">
+              <span class="drawer-keyboardKey">Ctrl</span> +
+              <span class="drawer-keyboardKey">I</span>
+            </td>
+            <td class="drawer-listCell">Italic</td>
+          </tr>
+          <tr>
+            <td class="drawer-listCell drawer-listCell--keyboard">
+              <span class="drawer-keyboardKey">*</span>
+              <span class="drawer-keyboardKey">Space</span>
+            </td>
+            <td class="drawer-listCell">Bulleted list</td>
+          </tr>
+          <tr>
+            <td class="drawer-listCell drawer-listCell--keyboard">
+              <span class="drawer-keyboardKey">Ctrl</span> +
+              <span class="drawer-keyboardKey">K</span>
+            </td>
+            <td class="drawer-listCell">
+              Turn into a link<span class="drawer-annotation"
+                >Works for text and images
+              </span>
+            </td>
+          </tr>
+          <tr>
+            <td class="drawer-listCell drawer-listCell--keyboard">
+              <span class="drawer-keyboardKey">1</span>
+              <span class="drawer-keyboardKey">.</span>
+              <span class="drawer-keyboardKey">Space</span>
+            </td>
+            <td class="drawer-listCell">Ordered list</td>
+          </tr>
+        </tbody>
+      </table>
       <table class="drawer-list">
         <tbody>
           <tr>
@@ -125,130 +128,131 @@ const windowsKeys = `
 `;
 
 const macKeys = `
+<a href="#" id="show" class="show-hide">Show Shortcuts</a>
 <div id="medium-shortcuts" class="fade-in">
-  <div class="drawer-content u-alignBlock js-drawerTip">
-    <table class="drawer-list">
-      <tbody>
-        <tr>
-          <td class="drawer-listCell drawer-listCell--keyboard">
-            <span class="drawer-keyboardKey">⌘</span> +
-            <span class="drawer-keyboardKey">Alt</span> +
-            <span class="drawer-keyboardKey">1</span>
-          </td>
-          <td class="drawer-listCell">Bigger header or title</td>
-        </tr>
-        <tr>
-          <td class="drawer-listCell drawer-listCell--keyboard">
-            <span class="drawer-keyboardKey">⌘</span> +
-            <span class="drawer-keyboardKey">Alt</span> +
-            <span class="drawer-keyboardKey">2</span>
-          </td>
-          <td class="drawer-listCell">Smaller header or subtitle</td>
-        </tr>
-        <tr>
-          <td class="drawer-listCell drawer-listCell--keyboard">
-            <span class="drawer-keyboardKey">⌘</span> +
-            <span class="drawer-keyboardKey">B</span>
-          </td>
-          <td class="drawer-listCell">Bold</td>
-        </tr>
-        <tr>
-          <td class="drawer-listCell drawer-listCell--keyboard">
-            <span class="drawer-keyboardKey">⌘</span> +
-            <span class="drawer-keyboardKey">Alt</span> +
-            <span class="drawer-keyboardKey">5</span>
-          </td>
-          <td class="drawer-listCell">
-            Quote<span class="drawer-annotation">Two styles of quotes </span>
-          </td>
-        </tr>
-        <tr>
-          <td class="drawer-listCell drawer-listCell--keyboard">
-            <span class="drawer-keyboardKey">⌘</span> +
-            <span class="drawer-keyboardKey">I</span>
-          </td>
-          <td class="drawer-listCell">Italic</td>
-        </tr>
-        <tr>
-          <td class="drawer-listCell drawer-listCell--keyboard">
-            <span class="drawer-keyboardKey">*</span>
-            <span class="drawer-keyboardKey">Space</span>
-          </td>
-          <td class="drawer-listCell">Bulleted list</td>
-        </tr>
-        <tr>
-          <td class="drawer-listCell drawer-listCell--keyboard">
-            <span class="drawer-keyboardKey">⌘</span> +
-            <span class="drawer-keyboardKey">K</span>
-          </td>
-          <td class="drawer-listCell">
-            Turn into a link<span class="drawer-annotation"
-              >Works for text and images
-            </span>
-          </td>
-        </tr>
-        <tr>
-          <td class="drawer-listCell drawer-listCell--keyboard">
-            <span class="drawer-keyboardKey">1</span>
-            <span class="drawer-keyboardKey">.</span>
-            <span class="drawer-keyboardKey">Space</span>
-          </td>
-          <td class="drawer-listCell">Ordered list</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-  <div class="drawer-content u-alignBlock js-drawerTip">
-    <table class="drawer-list">
-      <tbody>
-        <tr>
-          <td class="drawer-listCell drawer-listCell--keyboard">
-            <span class="drawer-keyboardKey">@</span> name or username
-          </td>
-          <td class="drawer-listCell">Link to another person’s profile</td>
-        </tr>
-        <tr>
-          <td class="drawer-listCell drawer-listCell--keyboard">
-            <span class="drawer-keyboardKey">T</span>
-            <span class="drawer-keyboardKey">K</span>
-          </td>
-          <td class="drawer-listCell">
-            Leave a TK reminder<span class="drawer-annotation"
-              ><a
-                class="link--underline"
-                href="https://medium.com/p/30dfec2921b5"
-                target="_blank"
-                >Learn more about TK</a
-              ></span
-            >
-          </td>
-        </tr>
-        <tr>
-          <td class="drawer-listCell drawer-listCell--keyboard">
-            <span class="drawer-keyboardKey">⌘</span> +
-            <span class="drawer-keyboardKey">Enter</span>
-          </td>
-          <td class="drawer-listCell">Add a separator</td>
-        </tr>
-        <tr>
-          <td class="drawer-listCell drawer-listCell--keyboard">
-            <span class="drawer-keyboardKey">\`</span> code
-          </td>
-          <td class="drawer-listCell">Code in a sentence</td>
-        </tr>
-        <tr>
-          <td class="drawer-listCell drawer-listCell--keyboard">
-            <span class="drawer-keyboardKey">\`</span>
-            <span class="drawer-keyboardKey">\`</span>
-            <span class="drawer-keyboardKey">\`</span>
-          </td>
-          <td class="drawer-listCell">Code block</td>
-        </tr>
-      </tbody>
-    </table>
+  <a href="#" id="hide" class="show-hide">Hide</a>  <div class="drawer-content u-alignBlock js-drawerTip">
+    <span class="title">Keyboard shortcuts</span>
+    <div class="table-container">
+      <table class="drawer-list">
+        <tbody>
+          <tr>
+            <td class="drawer-listCell drawer-listCell--keyboard">
+              <span class="drawer-keyboardKey">⌘</span> +
+              <span class="drawer-keyboardKey">Alt</span> +
+              <span class="drawer-keyboardKey">1</span>
+            </td>
+            <td class="drawer-listCell">Bigger header or title</td>
+          </tr>
+          <tr>
+            <td class="drawer-listCell drawer-listCell--keyboard">
+              <span class="drawer-keyboardKey">⌘</span> +
+              <span class="drawer-keyboardKey">Alt</span> +
+              <span class="drawer-keyboardKey">2</span>
+            </td>
+            <td class="drawer-listCell">Smaller header or subtitle</td>
+          </tr>
+          <tr>
+            <td class="drawer-listCell drawer-listCell--keyboard">
+              <span class="drawer-keyboardKey">⌘</span> +
+              <span class="drawer-keyboardKey">B</span>
+            </td>
+            <td class="drawer-listCell">Bold</td>
+          </tr>
+          <tr>
+            <td class="drawer-listCell drawer-listCell--keyboard">
+              <span class="drawer-keyboardKey">⌘</span> +
+              <span class="drawer-keyboardKey">Alt</span> +
+              <span class="drawer-keyboardKey">5</span>
+            </td>
+            <td class="drawer-listCell">
+              Quote<span class="drawer-annotation">Two styles of quotes </span>
+            </td>
+          </tr>
+          <tr>
+            <td class="drawer-listCell drawer-listCell--keyboard">
+              <span class="drawer-keyboardKey">⌘</span> +
+              <span class="drawer-keyboardKey">I</span>
+            </td>
+            <td class="drawer-listCell">Italic</td>
+          </tr>
+          <tr>
+            <td class="drawer-listCell drawer-listCell--keyboard">
+              <span class="drawer-keyboardKey">*</span>
+              <span class="drawer-keyboardKey">Space</span>
+            </td>
+            <td class="drawer-listCell">Bulleted list</td>
+          </tr>
+          <tr>
+            <td class="drawer-listCell drawer-listCell--keyboard">
+              <span class="drawer-keyboardKey">⌘</span> +
+              <span class="drawer-keyboardKey">K</span>
+            </td>
+            <td class="drawer-listCell">
+              Turn into a link<span class="drawer-annotation"
+                >Works for text and images
+              </span>
+            </td>
+          </tr>
+          <tr>
+            <td class="drawer-listCell drawer-listCell--keyboard">
+              <span class="drawer-keyboardKey">1</span>
+              <span class="drawer-keyboardKey">.</span>
+              <span class="drawer-keyboardKey">Space</span>
+            </td>
+            <td class="drawer-listCell">Ordered list</td>
+          </tr>
+        </tbody>
+      </table>
+      <table class="drawer-list">
+        <tbody>
+          <tr>
+            <td class="drawer-listCell drawer-listCell--keyboard">
+              <span class="drawer-keyboardKey">@</span> name or username
+            </td>
+            <td class="drawer-listCell">Link to another person’s profile</td>
+          </tr>
+          <tr>
+            <td class="drawer-listCell drawer-listCell--keyboard">
+              <span class="drawer-keyboardKey">T</span>
+              <span class="drawer-keyboardKey">K</span>
+            </td>
+            <td class="drawer-listCell">
+              Leave a TK reminder<span class="drawer-annotation"
+                ><a
+                  class="link--underline"
+                  href="https://medium.com/p/30dfec2921b5"
+                  target="_blank"
+                  >Learn more about TK</a
+                ></span
+              >
+            </td>
+          </tr>
+          <tr>
+            <td class="drawer-listCell drawer-listCell--keyboard">
+              <span class="drawer-keyboardKey">⌘</span> +
+              <span class="drawer-keyboardKey">Enter</span>
+            </td>
+            <td class="drawer-listCell">Add a separator</td>
+          </tr>
+          <tr>
+            <td class="drawer-listCell drawer-listCell--keyboard">
+              <span class="drawer-keyboardKey">\`</span> code
+            </td>
+            <td class="drawer-listCell">Code in a sentence</td>
+          </tr>
+          <tr>
+            <td class="drawer-listCell drawer-listCell--keyboard">
+              <span class="drawer-keyboardKey">\`</span>
+              <span class="drawer-keyboardKey">\`</span>
+              <span class="drawer-keyboardKey">\`</span>
+            </td>
+            <td class="drawer-listCell">Code block</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </div>
-
 `;
 
 const platform = window.navigator.userAgentData.platform.toLowerCase();
@@ -260,10 +264,29 @@ if (isWindows) {
     .getElementsByTagName("body")[0]
     .insertAdjacentHTML("beforeend", windowsKeys);
 } else if (isMac) {
-  console.log("mac");
   document
     .getElementsByTagName("body")[0]
     .insertAdjacentHTML("beforeend", macKeys);
 } else {
   alert("Medium Keyboard Shortcuts could not detect keyboard layout.");
 }
+
+const shortcuts = document.getElementById("medium-shortcuts");
+const hideButton = document.getElementById("hide");
+const showButton = document.getElementById("show");
+
+hideButton.addEventListener("click", () => {
+  showButton.classList.remove("fade-out");
+  showButton.classList.add("fade-in");
+
+  shortcuts.classList.remove("fade-in");
+  shortcuts.classList.add("fade-out");
+});
+
+showButton.addEventListener("click", () => {
+  shortcuts.classList.remove("fade-out");
+  shortcuts.classList.add("fade-in");
+
+  showButton.classList.remove("fade-in");
+  showButton.classList.add("fade-out");
+});
